bcb13589bccc053196dcd6aedf43a91eb39aae61
Anirudh Joshi <120326069+anirudhJoshi1112@users.noreply.github.com>
2025-09-19 23:58:30 +0530

    Basics of List

---
+++ 1.py
@@ -0,0 +1,18 @@
+thisList = ["Apple", "Ball", "Cat"]
+print(thisList)
+print(len(list))  
+
+
+#data types in list
+thisList = ["Car","Bike"]
+thisList = [1,2,3]
+thisList = [True, False,True]
+
+#A list can contain different data types:
+#Example
+#A list with strings, integers and boolean values:
+
+list1 = ["abc", 34, True, 40, "male"]
+
+
+
