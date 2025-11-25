c70dd5a0f6d29f48692caec12b3d3b3d85508570
Anirudh Joshi <120326069+anirudhJoshi1112@users.noreply.github.com>
2025-11-25 11:37:42 +0530

    Contains Duplicate
    Check question number for detailed explanation.
    
    1. Brute Force ->
    if you want to compare an element with outer loop (i), (here j comparing with i) So  remember don't directly write j=1, instead of that use j=i+1

---
+++ LeetCode217.cpp
@@ -0,0 +1,30 @@
+// Brute Force -> O(N^2)
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        for(int i=0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]==nums[j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
+
+// Better  -> O(N(logN))
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+
+        sort(nums.begin(),nums.end());
+
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]==nums[i-1]){
+                return true;
+            }
+        }
+        return false;
+    }
+};
